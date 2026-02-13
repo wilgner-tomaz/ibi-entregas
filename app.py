73d980c488ee5792affc8741113f45f23067d266
Wilgner Tomaz da Silva <104664889+wilgner-tomaz@users.noreply.github.com>
2026-02-13 00:59:01 -0300

    primeiro deploy

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, request, redirect, send_file
+import qrcode
+
+app = Flask(__name__)
+
+ANDROID_LINK = "https://play.google.com/store/apps/details?id=br.com.client.ibientregas"
+IOS_LINK = "https://apps.apple.com/br/app/ibi-entregas/id6743942785"
+
+FIXED_URL = "https://ibientregas.com/app"
+
+
+@app.route("/")
+def home():
+    return send_file("qrcode_fixo.png", mimetype="image/png")
+
+
+@app.route("/app")
+def smart_redirect():
+    ua = request.headers.get("User-Agent", "").lower()
+
+    if "android" in ua:
+        return redirect(ANDROID_LINK, code=302)
+
+    if any(x in ua for x in ["iphone", "ipad", "ipod"]):
+        return redirect(IOS_LINK, code=302)
+
+    return "<h2>Abra este QR Code no celular 📱</h2>"
+
+
+def generate_static_qrcode():
+    img = qrcode.make(FIXED_URL)
+    img.save("qrcode_fixo.png")
+    print("QR Code gerado: qrcode_fixo.png")
+
+
+if __name__ == "__main__":
+    generate_static_qrcode()
+    app.run(host="0.0.0.0", port=5000)
